e05a25ff69250921a770160600bfd666b144cb5c
Saki Celik <87222451+sakiceliks@users.noreply.github.com>
2024-06-14 17:34:45 +0300

    hay

--- src/components/About/InsightRoll.js
+++ src/components/About/InsightRoll.js
@@ -4,11 +4,12 @@ const InsightRoll = ({ insights }) => {
   return (
     <div className="w-full bg-accent dark:bg-accentDark text-light dark:text-dark whitespace-nowrap overflow-hidden">
       <div className="animate-roll  w-full py-2 sm:py-3 flex items-center justify-center capitalize font-semibold tracking-wider text-sm sm:text-base">
-        {insights.map((text) => (
-          <div key={index}>
-            {text} <span className="px-4">|</span>{" "}
-          </div>
-        ))}
+      {insights.map((text, index) => (
+  <div key={index}>
+    {text} <span className="px-4">|</span>{" "}
+  </div>
+))}
+
       </div>
     </div>
   );
